a27b91b53784f2dce47f7614fb6cdf8f385487fd
jameshill-cloud <james.hill@solirius.com>
2026-06-10 16:26:09 +0100

    fix test files/directories being copied to dist directory

--- scripts/copy-assets.ts
+++ scripts/copy-assets.ts
@@ -1,5 +1,5 @@
-import { copyFileSync, mkdirSync, cpSync } from "fs";
-import { join } from "path";
+import { copyFileSync, mkdirSync, cpSync, statSync } from "fs";
+import { join, basename } from "path";
 
 const govukDistPath = join(
   __dirname,
@@ -64,7 +64,22 @@ try {
 try {
   cpSync(join(__dirname, "../src/views"), join(__dirname, "../dist/views"), {
     recursive: true,
-    force: true,
+    filter: (src) => {
+      const name = basename(src);
+
+      if (name === "__tests__") {
+        return false;
+      }
+
+      const stat = statSync(src);
+
+      if (stat.isDirectory()) {
+        return true;
+      }
+
+      const isNjk = src.endsWith(".njk");
+      return isNjk;
+    },
   });
   console.log("✓ Copied views/");
 } catch (err: any) {
